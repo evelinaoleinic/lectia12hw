bbf9621014afeafe447dfbd0854adb6a916127d7
evelinaoleinic <evelina.oleinic1@gmail.com>
2024-03-04 13:26:06 +0200

    ex

---
+++ tema12.js
@@ -0,0 +1,51 @@
+var ex1 = 6 //number
+let ex2 = 'text' //string
+let ex3 = true //bool
+let ex4 = undefined //undefined
+let ex5 = null //null
+let ex6 = BigInt(123456) //BigInt
+// let ex7 = Symbol(' ')  //symbol
+
+
+// console.log(ex1)
+// console.log(ex2)
+// console.log(ex3)
+// console.log(ex4)
+// console.log(ex5)
+// console.log(ex6)
+
+let num1 = 5
+let num2 = 7
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+
+console.log(num2++)
+console.log(num2)
+
+console.log(++num1)
+
+console.log(num1--)
+
+console.log(--num2)
+
+console.log(num2**4)
+console.log(num1**2)
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 <= num2)
+console.log(num1 >= num2)
+
+console.log(!ex3)
+console.log(!ex4)
+console.log(!ex2)
+
+console.log(num1 == num2)
+console.log(num1 === num2)
+
+
+
+
+
